0632717e6d2b61abfa01897271bab24e3cb4b3e7
Intelligent UAS <61607236+QuetzalCpp@users.noreply.github.com>
2023-01-19 01:00:55 -0600

    Add files via upload

---
+++ util/compute_image_mean.py
@@ -0,0 +1,71 @@
+# python util/compute_image_mean.py --dataroot datasets/KingsCollege --height 256 --width 455 --save_resized_imgs
+import argparse
+import numpy as np
+from os.path import join as jpath
+from PIL import Image
+
+
+def params():
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--dataroot', type=str, default='datasets/KingsCollege', help='dataset root')
+    parser.add_argument('--height', type=int, default=256, help='image height')
+    parser.add_argument('--width', type=int, default=455, help='image width')
+    parser.add_argument('--save_resized_imgs', action="store_true", default=False, help='save resized train/test images [height, width]')
+    return parser.parse_args()
+
+args = params()
+dataroot = args.dataroot
+imsize = [args.height, args.width] # (H, W)
+# =========== Resize images to train and save the mean ============================
+"""imlist =  np.loadtxt(jpath(dataroot, 'dataset_train.txt'),
+                    dtype=str, delimiter=' ', skiprows=3, usecols=(0))
+mean_image = np.zeros((imsize[0], imsize[1], 3), dtype=np.float)
+for i, impath in enumerate(imlist):
+    #print('[%d/%d]:%s' % (i+1, len(imlist), impath), end='\r')
+    print('[%d/%d]:%s' % (i+1, len(imlist), impath))
+    image = Image.open(jpath(dataroot, impath)).convert('RGB')
+    image = image.resize((imsize[1], imsize[0]), Image.BICUBIC)
+    mean_image += np.array(image).astype(np.float)
+
+    # save resized training images
+    if args.save_resized_imgs:
+        image.save(jpath(dataroot, impath))
+print()
+mean_image /= len(imlist)
+Image.fromarray(mean_image.astype(np.uint8)).save(jpath(dataroot, 'mean_image.png'))
+np.save(jpath(dataroot, 'mean_image.npy'), mean_image)"""
+
+# =========== Resize images to test ============================
+# save resized test images
+if args.save_resized_imgs:
+    imlist =  np.loadtxt(jpath(dataroot, 'dataset_scene2_validation2_test.txt'),
+    #imlist =  np.loadtxt(jpath(dataroot, 'scene1_validation1_test.txt'),
+    #imlist =  np.loadtxt(jpath(dataroot, 'dataset_test.txt'),
+                        dtype=str, delimiter=' ', skiprows=3, usecols=(0))
+    for i, impath in enumerate(imlist):
+        print('[%d/%d]:%s' % (i+1, len(imlist), impath))
+        #print('[%d/%d]:%s' % (i+1, len(imlist), impath), end='\r')
+        image = Image.open(jpath(dataroot, impath)).convert('RGB')
+        image = image.resize((imsize[1], imsize[0]), Image.BICUBIC)
+        image.save(jpath(dataroot, impath))
+    print()
+
+    """imlist =  np.loadtxt(jpath(dataroot, 'scene1_validation2_test.txt'),
+                        dtype=str, delimiter=' ', skiprows=3, usecols=(0))
+    for i, impath in enumerate(imlist):
+        print('[%d/%d]:%s' % (i+1, len(imlist), impath))
+        #print('[%d/%d]:%s' % (i+1, len(imlist), impath), end='\r')
+        image = Image.open(jpath(dataroot, impath)).convert('RGB')
+        image = image.resize((imsize[1], imsize[0]), Image.BICUBIC)
+        image.save(jpath(dataroot, impath))
+    print()
+
+    imlist =  np.loadtxt(jpath(dataroot, 'scene1_validation3_test.txt'),
+                        dtype=str, delimiter=' ', skiprows=3, usecols=(0))
+    for i, impath in enumerate(imlist):
+        print('[%d/%d]:%s' % (i+1, len(imlist), impath))
+        #print('[%d/%d]:%s' % (i+1, len(imlist), impath), end='\r')
+        image = Image.open(jpath(dataroot, impath)).convert('RGB')
+        image = image.resize((imsize[1], imsize[0]), Image.BICUBIC)
+        image.save(jpath(dataroot, impath))
+    print()"""
